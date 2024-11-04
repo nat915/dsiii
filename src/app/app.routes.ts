4f314ca9fa08d38cce308e3016eeb881b8f1d86b
natless77 <lastnagusarname@gmail.com>
2024-11-03 23:30:46 -0500

    fdfd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { SingInComponent } from './components/sing-in/sing-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MaintenanceComponent } from './components/maintenance/maintenance.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'singIn', component: SingInComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'maintenance', component: MaintenanceComponent},
+  {path: 'errorPage', component: ErrorPageComponent},
+  {path: '**', redirectTo:'/errorPage',pathMatch:'full'}
+
+];
---
+++ src/app/components/sing-in/sing-in.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sing-in',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './sing-in.component.html',
+  styleUrl: './sing-in.component.css'
+})
+export class SingInComponent {
+
+}
